e2f254c5d769fc7951219a05ac509e87c46e4496
usereri <eryk.jamroz@gmail.com>
2026-04-17 22:09:23 +0200

    Deploy script updated

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -29,40 +29,28 @@ contract Deploy is Script {
         console.log("Balance:", deployer.balance);
 
         vm.startBroadcast(deployerPrivateKey);
-
-        // 1. Deploy UserProfileNFT
         UserProfileNFT userProfileNFT = new UserProfileNFT();
         console.log("UserProfileNFT deployed at:", address(userProfileNFT));
 
-        // 2. Deploy MeetingAttendanceNFT
         MeetingAttendanceNFT meetingAttendanceNFT = new MeetingAttendanceNFT();
         console.log(
             "MeetingAttendanceNFT deployed at:",
             address(meetingAttendanceNFT)
         );
 
-        // 3. Deploy CommunityVault
-        CommunityVault communityVault = new CommunityVault(address(0)); // Registry set later
+        CommunityVault communityVault = new CommunityVault(address(0));
         console.log("CommunityVault deployed at:", address(communityVault));
 
-        // 4. Deploy TreasuryContract
-        TreasuryContract treasuryContract = new TreasuryContract(address(0)); // Registry set later
+        TreasuryContract treasuryContract = new TreasuryContract(address(0));
         console.log("TreasuryContract deployed at:", address(treasuryContract));
 
-        // 5. Deploy ContractRegistry with deployer as owner
         ContractRegistry registry = new ContractRegistry(deployer);
         console.log("ContractRegistry deployed at:", address(registry));
 
-        // Configure all contracts with registry addresses
         registry.setUserProfileNFT(address(userProfileNFT));
         registry.setMeetingAttendanceNFT(address(meetingAttendanceNFT));
         registry.setTreasuryContract(address(treasuryContract));
         registry.setCommunityVault(address(communityVault));
-
-        // Update CommunityVault and TreasuryContract with registry
-        // (Note: these contracts don't have setter functions, so we pass registry at constructor)
-        // For production, consider adding setters or re-deploying with correct registry
-
         vm.stopBroadcast();
 
         console.log("");
@@ -73,7 +61,6 @@ contract Deploy is Script {
         console.log("CommunityVault:", address(communityVault));
         console.log("ContractRegistry:", address(registry));
 
-        // Generate JSON output for frontend
         console.log("");
         console.log("=== contracts.json ===");
         console.log(
